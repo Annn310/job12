83b90d6f9660d4a990f05dede77d36554bdd16ab
job12 <rgagas100@gmail.com>
2022-09-20 09:18:43 +0700

    first commit

---
+++ app/src/main/java/com/example/job12/profilsmk.kt
@@ -0,0 +1,11 @@
+package com.example.job12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profilsmk : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profilsmk)
+    }
+}
